93afae02e5463751c88e81be80d6e8bf156d5622
Angel <angcorado4@gmail.com>
2018-10-03 17:47:29 -0600

    Extend - Detalle - Url

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def listar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/listar.html', {'post': post})
+
 
 # Create your views here.
