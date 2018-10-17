888eabb0d229d46f258e94e26816dbc72e4c41b6
Angel <angcorado4@gmail.com>
2018-10-17 15:58:03 -0600

    Formulario Agregar - Editar

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.listar, name='listar'),
+    url(r'^post/nueva/$', views.agregar, name='agregar'),
+    url(r'^post/(?P<pk>[0-9]+)/editar/$', views.editar, name='editar'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
+from .form import PostForm
+
 
 def post_list(request):
     posts = Post.objects.all()
@@ -10,5 +12,32 @@ def listar(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/listar.html', {'post': post})
 
+def agregar(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('listar', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/agregar.html', {'form': form})
+
+
+
+def editar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('listar', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/agregar.html', {'form': form})
 
 # Create your views here.
