e3fdaa3078693b564c37763df7ba3c987774bd0a
Angel <angcorado4@gmail.com>
2018-10-17 17:18:58 -0600

    Borrador - Eliminar - Post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.listar, name='listar'),
     url(r'^post/nueva/$', views.agregar, name='agregar'),
     url(r'^post/(?P<pk>[0-9]+)/editar/$', views.editar, name='editar'),
+    url(r'^borrador/$', views.borrador, name='borrador'),
+    url(r'^post/(?P<pk>\d+)/publicados/$', views.publicar, name='publicar'),
+    url(r'^post/(?P<pk>\d+)/eliminar/$', views.eliminar, name='eliminar'),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .form import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def listar(request, pk):
@@ -18,7 +18,6 @@ def agregar(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('listar', pk=post.pk)
     else:
@@ -40,4 +39,21 @@ def editar(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/agregar.html', {'form': form})
 
+def borrador(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/borrador.html', {'posts': posts})
+
+def publicar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('listar', pk=pk)
+
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()         
+
+def eliminar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('list')
 # Create your views here.
